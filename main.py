262d33801b9b0190b23de44470cc91bb794bc0fe
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-08-21 10:46:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ DB_HOST = "127.0.0.1"
 DB_PORT = 3306
 DB_NAME = "car_customizer"
 DB_USER = "root"
-DB_PASSWORD = "P14Y3R"
+DB_PASSWORD = "" #Insert your password here
 
 
 # ============================================================
